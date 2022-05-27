03b213c189f402bff8b6cd4706fe70eb09dd372b
Nong <carrynong2@gmail.com>
2022-05-27 16:58:42 +0700

    Attribute Binding

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <section>
+    <img :src="picture" :width="size" :height="size" />
     <h1>ชื่อ-นามสกุล : {{ getFullName() }}</h1>
     <h1>อายุ : {{ age }} ปี</h1>
     <p>ที่อยู่ : <span v-html="address"></span></p>
+    <a :href="social" target="_blank">Facebook</a>
   </section>
 </template>
 
@@ -15,6 +17,9 @@ export default {
       lastName: "CarryNong",
       age: 25,
       address: "<i>กรุงเทพมหานคร</i>",
+      picture: "https://cdn-icons-png.flaticon.com/512/6970/6970635.png",
+      size: 150,
+      social: "https://www.facebook.com/monthon.mukkun/",
     };
   },
   methods: {
