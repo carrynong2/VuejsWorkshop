ec9986d24fbd6611e9352fedd208b5c8b2b717b1
Nong <carrynong2@gmail.com>
2022-05-28 20:46:54 +0700

    Add Render Condition

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,19 @@
   <section>
     <img :src="picture" :width="size" :height="size" ref="imageEl" /><br />
 
-    <form @submit.prevent="submitForm">
-      <label>ป้อนชื่อเล่น:</label>
-      <input type="text" ref="nickNameEl" />
-      <button type="submit">บันทึก</button>
-    </form>
     <h1>ชื่อ-นามสกุล : {{ getFullName() }}</h1>
-    <h1>ชื่อเล่น : {{ nickName }}</h1>
     <h1>อายุ : {{ age }} ปี</h1>
     <p>ที่อยู่ : <span v-html="address"></span></p>
     <p>Social : <a :href="social" target="_blank">Facebook</a></p>
-    <p>งานอดิเรก:</p>
-    <ul>
-      <li>{{ hobby[0] }}</li>
-      <li>{{ hobby[1] }}</li>
-      <li>{{ hobby[2] }}</li>
-    </ul>
+    <p v-if="hobby.length === 0">ไม่มีงานอดิเรก</p>
+    <div v-else>
+      <p>งานอดิเรก:</p>
+      <ul>
+        <li>{{ hobby[0] }}</li>
+        <li>{{ hobby[1] }}</li>
+        <li>{{ hobby[2] }}</li>
+      </ul>
+    </div>
     <p>ข้อมูลพื้นฐาน :</p>
     <ul>
       <li>เพศ: {{ general.gender }}</li>
@@ -25,9 +22,6 @@
       <li>ส่วนสูง: {{ general.height }} cm</li>
       <li>โรคประจำตัว: {{ general.status }}</li>
     </ul>
-    <button @click="showData">คลิกเพื่อดูข้อมูล</button>
-    <button @click.ctrl="increment(10)">เพิ่ม</button>
-    <button @click.middle="decrement(5)">ลด</button>
   </section>
 </template>
 
@@ -38,31 +32,18 @@ export default {
     return {
       firstName: "Dev",
       lastName: "CarryNong",
-      nickName: "",
       age: 25,
       address: "<i>กรุงเทพมหานคร</i>",
       picture: "https://cdn-icons-png.flaticon.com/512/6970/6970635.png",
       size: 150,
       social: "https://www.facebook.com/monthon.mukkun/",
-      hobby: ["ทำสวน", "เล่นเกม", "ฟังเพลง"],
+      hobby: ["ทำสวน"],
       general: { gender: "ชาย", weight: 70.4, height: 170, status: false },
     };
   },
   methods: {
     getFullName() {
       return `${this.firstName}  ${this.lastName}`;
-    },
-    showData() {
-      alert(this.firstName);
-    },
-    increment(value) {
-      this.age += value;
-    },
-    decrement(value) {
-      this.age -= value;
-    },
-    submitForm() {
-      this.nickName = this.$refs.nickNameEl.value;
     },
   },
 };
